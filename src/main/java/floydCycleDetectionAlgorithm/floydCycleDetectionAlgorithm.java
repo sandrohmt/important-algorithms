c94948794c9452c0063d94c0aa41cc116b0a3ea4
sandrohmt <sandrohmt@gmail.com>
2025-03-26 15:45:48 -0300

    Implemented Floyd's cycle detection algorithm

---
+++ src/main/java/floydCycleDetectionAlgorithm/floydCycleDetectionAlgorithm.java
@@ -0,0 +1,87 @@
+package floydCycleDetectionAlgorithm;
+
+
+public class floydCycleDetectionAlgorithm {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void append (int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+    }
+
+    public void createCycle(int position) {
+        if (head == null) return;
+
+        Node temp = head;
+        Node cycleNode = null;
+        int count = 1;
+
+        while (temp.next != null) {
+            if (position == count) {
+                cycleNode = temp;
+            }
+            temp = temp.next;
+            count++;
+        }
+        temp.next = cycleNode;
+    }
+
+    public boolean floydDetectCycle() {
+        Node slow = head;
+        Node fast = head;
+        int count = 1;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) { // Se os ponteiros se encontrarem, há um ciclo
+                slow = head;
+                while (slow != fast) {
+                    count++;
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                System.out.println("O ponto de entrada do ciclo é a " + count + "° posição");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        floydCycleDetectionAlgorithm list = new floydCycleDetectionAlgorithm();
+
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
+
+        list.createCycle(1);
+
+        if (list.floydDetectCycle()) {
+            System.out.println("Ciclo detectado na lista!");
+        } else {
+            System.out.println("Nenhum ciclo detectado!");
+        }
+    }
+}
